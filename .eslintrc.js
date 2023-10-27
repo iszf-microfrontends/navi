0b97f6157c03c412e97e551a7fe69cd37da958a8
Sviatoslav <sviatoslavnazin@gmail.com>
2023-10-27 18:08:50 +0800

    install effector and effector tools

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
-  extends: ['@iszf-microfrontends/eslint-config'],
-  plugins: ['react-refresh'],
+  extends: ['@iszf-microfrontends/eslint-config', 'plugin:effector/recommended', 'plugin:effector/react', 'plugin:effector/scope'],
+  plugins: ['react-refresh', 'effector'],
   ignorePatterns: ['dist', 'webpack.config.js', '.eslintrc.js'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
